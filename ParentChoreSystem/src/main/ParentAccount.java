7d5d34079208e984013bae0f8e41049c61b39ec8
218928960 <willvm24@my.yorku.ca>
2024-02-07 13:00:23 -0500

    Update ParentAccount.java

--- ParentChoreSystem/src/main/ParentAccount.java
+++ ParentChoreSystem/src/main/ParentAccount.java
@@ -1,15 +1,25 @@
 package main;
 
+import java.util.List;
+
 public class ParentAccount {
 	
 	private String choreName;
+	private double payment;
+	private List<ChoreEditing> chores;
 	
 	public ParentAccount(String choreName) {
 		this.choreName= choreName;
+		this.payment = 0.0;
+		
 	}
 
 	public String getChoreName() {
 		return choreName;
+	}
+
+	public double getPayment() {
+		return payment;
 	}
 	
 
