c68f2b9507e571c2931084a3ba617e11a55b1756
addu13 <adam04@my.yorku.ca>
2024-02-08 20:11:26 -0500

    Added methods to ChoresSort

---
+++ ParentChoreSystem/src/choresModule/ChoresSort.java
@@ -0,0 +1,83 @@
+package choresModule;
+
+//import java.util.ArrayList;
+//import java.util.Collections;
+//import java.util.List;
+
+public class ChoresSort {
+	
+	
+	ChoresList chores = new ChoresList();
+
+
+		
+	////import all chores
+	////import all categories
+	
+	/*//
+	SORTING & FILTERING
+	///*
+	 * Sort Alphabetically
+	 * Sort by amount of time
+	 * Sort by payment amount
+	 * 
+	 * Filter Completed/Not Completed
+	 * Filter paid or not
+	 * Filter by Category
+	 * 
+	 */
+public void sortAlphabetically() {
+	
+    //Collections.sort(chores);
+	
+	//return sorted list
+
+}
+
+public void sortByTime() {
+	//chore.getTime();
+	
+	//import 
+	
+	//return sorted list
+
+}
+
+public void sortByPayment() {
+	//chore.getPayment();
+	
+	//import 
+	
+	//return sorted list
+
+}
+
+public void filterCompleted() {
+	//chore.isCompleted();
+	
+	//import 
+	
+	//return filtered list
+
+}
+
+public void filterPaid() {
+	//chore.isPaid();
+	
+	//import 
+
+	//return filtered list
+
+}
+
+public void filterCatogory() {
+	//chore.getCatogory();
+	
+	//import 
+	
+	//return filtered list
+
+
+}
+	
+}
