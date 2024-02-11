adb13fa6837f5f9ff72cef9cac98e8b2d8d5a013
vrushank2001 <vrushankv1990@gmail.com>
2024-02-11 02:05:12 -0500

    Update methods in ChoresSort.java

--- ParentChoreSystem/src/choresModule/ChoresSort.java
+++ ParentChoreSystem/src/choresModule/ChoresSort.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class ChoresSort {
 	
 	
-	public ArrayList<Chore> chores = new ArrayList<Chore>();
+	//public ArrayList<Chore> chores = new ArrayList<Chore>();
 
 
 	//Might have to make a constructor for this class
@@ -24,94 +24,153 @@ public class ChoresSort {
 	 * Filter by Category
 	 * 
 	 */
-public void sortAlphabetically() {
-	//making a string arrayList because collection sort only works on java made objects
-	ArrayList<String> choresString = new ArrayList<>();
-	for (int i=0;i<chores.size();i++) {
-		//using a for loop to convert each element from chores into a string
-		choresString.add(chores.get(i).toString());
+	
+	public static List<Chore> sortAlphabetically(List<Chore> chores) {
+	//	//making a string arrayList because collection sort only works on java made objects
+	//	ArrayList<String> choresString = new ArrayList<>();
+	//	for (int i=0;i<chores.size();i++) {
+	//		//using a for loop to convert each element from chores into a string
+	//		choresString.add(chores.get(i).toString());
+	//	}
+	//	//sorting the string arrayList by alphabets
+	//    Collections.sort(choresString);
+		
+		List<Chore> secondaryList = new ArrayList<>(chores);
+		Collections.sort(secondaryList, Comparator.comparing(Chore::getName));
+		return secondaryList;
 	}
-	//sorting the string arrayList by alphabets
-    Collections.sort(choresString);
-}
-
-public void sortByTime() {
-	//chore.getTime();
 	
-	//import 
-	
-	//return sorted list
-	ArrayList<Double> choresTime = new ArrayList<>();
-	for (int i=0;i<chores.size();i++) {
-		choresTime.add(chores.get(i).getTime());
+	public static List<Chore> sortByTime(List<Chore> chores) {
+		//chore.getTime();
+		
+		//import 
+		
+		//return sorted list
+	//	ArrayList<Double> choresTime = new ArrayList<>();
+	//	for (int i=0;i<chores.size();i++) {
+	//		choresTime.add(chores.get(i).getTime());
+	//	}
+	//	Collections.sort(choresTime);
+		
+		List<Chore> secondaryList = new ArrayList<>(chores);
+		Collections.sort(secondaryList, Comparator.comparing(Chore::getTime));
+		return secondaryList;
+		
 	}
-	Collections.sort(choresTime);
-}
-
-public void sortByPayment() {
-	//chore.getPayment();
 	
-	//import 
-	
-	//return sorted list
-	ArrayList<Double> choresPayment = new ArrayList<>();
-	for (int i=0;i<chores.size();i++) {
-		choresPayment.add(chores.get(i).getPayment());
+	public static List<Chore> sortByPayment(List<Chore> chores) {
+		//chore.getPayment();
+		
+		//import 
+		
+		//return sorted list
+	//	ArrayList<Double> choresPayment = new ArrayList<>();
+	//	for (int i=0;i<chores.size();i++) {
+	//		choresPayment.add(chores.get(i).getPayment());
+	//	}
+	//	Collections.sort(choresPayment);
+		
+		List<Chore> secondaryList = new ArrayList<>(chores);
+		Collections.sort(secondaryList, Comparator.comparing(Chore::getPayment));
+		return secondaryList;
 	}
-	Collections.sort(choresPayment);
-}
-
-public void filterCompleted() {
-	//chore.isCompleted();
 	
-	//import 
-	
-	//return filtered list
-	
-	/*We can add an option where if a person clicks on a button
-	 * It will display the ones that are completed and vice versa.
-	 * For that we need some more code classes, and once that is done
-	 * we can edit this method by adding an if statement*/
-	ArrayList<Boolean> choresComplete = new ArrayList<>();
-	for (int i=0;i<chores.size();i++) {
-		choresComplete.add(chores.get(i).isCompleted());
+	public static List<Chore> filterCompleted(List<Chore> chores) {
+		//chore.isCompleted();
+		
+		//import 
+		
+		//return filtered list
+		
+		/*We can add an option where if a person clicks on a button
+		 * It will display the ones that are completed and vice versa.
+		 * For that we need some more code classes, and once that is done
+		 * we can edit this method by adding an if statement*/
+	//	ArrayList<Boolean> choresComplete = new ArrayList<>();
+	//	for (int i=0;i<chores.size();i++) {
+	//		choresComplete.add(chores.get(i).isCompleted());
+	//	}
+	//	Collections.sort(choresComplete);
+		
+		List<Chore> secondaryList = new ArrayList<>();
+		for(int i = 0; i < chores.size(); i++) {
+			if(chores.get(i).isCompleted()) {
+				secondaryList.add(chores.get(i));
+			}
+		}
+		return secondaryList;
 	}
-	Collections.sort(choresComplete);
-}
-
-public void filterPaid() {
-	//chore.isPaid();
 	
-	//import 
-
-	//return filtered list
-	/*We can add an option where if a person clicks on a button
-	 * It will display the ones that are paid and vice versa.
-	 * For that we need some more code classes, and once that is done
-	 * we can edit this method by adding an if statement*/
-	ArrayList<Boolean> choresPaid = new ArrayList<>();
-	for (int i=0;i<chores.size();i++) {
-		choresPaid.add(chores.get(i).isPaid());
+	public static List<Chore> filterNotCompleted(List<Chore> chores) {
+		
+		List<Chore> secondaryList = new ArrayList<>();
+		for(int i = 0; i < chores.size(); i++) {
+			if(!chores.get(i).isCompleted()) {
+				secondaryList.add(chores.get(i));
+			}
+		}
+		return secondaryList;
 	}
-	Collections.sort(choresPaid);
-}
-
-public void filterCatogory() {
-	//chore.getCatogory();
 	
-	//import 
+	public static List<Chore> filterPaid(List<Chore> chores) {
+		//chore.isPaid();
+		
+		//import 
 	
-	//return filtered list
-	/*We can add an option where if a person clicks on a button
-	 * It will display the specified category.
-	 * For that we need some more code classes, and once that is done
-	 * we can edit this method by adding an if statement*/
-	ArrayList<String> choresCategory = new ArrayList<>();
-	for (int i=0;i<chores.size();i++) {
-		choresCategory.add(chores.get(i).getCategory());
+		//return filtered list
+		/*We can add an option where if a person clicks on a button
+		 * It will display the ones that are paid and vice versa.
+		 * For that we need some more code classes, and once that is done
+		 * we can edit this method by adding an if statement*/
+	//	ArrayList<Boolean> choresPaid = new ArrayList<>();
+	//	for (int i=0;i<chores.size();i++) {
+	//		choresPaid.add(chores.get(i).isPaid());
+	//	}
+	//	Collections.sort(choresPaid);
+		
+		List<Chore> secondaryList = new ArrayList<>();
+		for(int i = 0; i < chores.size(); i++) {
+			if(chores.get(i).isPaid()) {
+				secondaryList.add(chores.get(i));
+			}
+		}
+		return secondaryList;
 	}
-	Collections.sort(choresCategory);
-
-}
+	
+	public static List<Chore> filterNotPaid(List<Chore> chores) {
+		
+		List<Chore> secondaryList = new ArrayList<>();
+		for(int i = 0; i < chores.size(); i++) {
+			if(!chores.get(i).isPaid()) {
+				secondaryList.add(chores.get(i));
+			}
+		}
+		return secondaryList;
+	}
+	
+	public static List<Chore> filterCategory(String category, List<Chore> chores) {
+		//chore.getCatogory();
+		
+		//import 
+		
+		//return filtered list
+		/*We can add an option where if a person clicks on a button
+		 * It will display the specified category.
+		 * For that we need some more code classes, and once that is done
+		 * we can edit this method by adding an if statement*/
+	//	ArrayList<String> choresCategory = new ArrayList<>();
+	//	for (int i=0;i<chores.size();i++) {
+	//		choresCategory.add(chores.get(i).getCategory());
+	//	}
+	//	Collections.sort(choresCategory);
+	
+		List<Chore> secondaryList = new ArrayList<>();
+		for(int i = 0; i < chores.size(); i++) {
+			if(chores.get(i).getCategory().equals(category)) {
+				secondaryList.add(chores.get(i));
+			}
+		}
+		return secondaryList;
+	}
 	
 }
