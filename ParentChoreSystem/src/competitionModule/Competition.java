c119f1deb27607d3ed4ca4fa4c79a4149a30ac16
NathanBinu <Nathanbinu011@gmail.com>
2024-02-12 22:51:17 -0500

    Update Competition.java

--- ParentChoreSystem/src/competitionModule/Competition.java
+++ ParentChoreSystem/src/competitionModule/Competition.java
@@ -8,31 +8,21 @@ import choresModule.Chore;
 
 
 public class Competition {
-	
-	public int rewardPoints; //storing rewards points 
+public int rewardPoints; //storing rewards points 
 	
 	public Competition() {
 		rewardPoints = 0;  //initializing reward points 
 	}
 	
-		public List<Chore> pointCalculation() {
-
-		int totalpoints = 0;
-		
-		ArrayList<Double> chore = new ArrayList<>();
-		for (double i=0;i<chore.size();i++) {
-					chore.add(chore.get(i).getPayment());
-				}
-		
-		double hoursWorked = chores.get(1).getTime()
-		double hoursWorked = child.getHoursWorked();
-		rewardPoints += (int) (hoursWorked / 10);
+	public int calculatePoints(List<Chore> chores) {
 		for (Chore chore : chores) {
-			int chorePoints = calculateChorePoints(chore);
-			totalpoints += chorePoints;
+			double time = chore.getTime();
+			rewardPoints += time / 10;
 		}
-		return totalpoints
-		
-	}}
-
+		return rewardPoints;
+	}
+	
+	public int getRewardPoints() {
+		return rewardPoints;
+	}
 }
