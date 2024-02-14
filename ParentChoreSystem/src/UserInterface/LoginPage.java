59645f9435a77203f5d7fb0e4a4e27d1df3ee1dd
Harmandeep Arneja <arneja.harry@gmail.com>
2024-02-13 21:15:47 -0500

    Single unnecessary line deletion

--- ParentChoreSystem/src/UserInterface/LoginPage.java
+++ ParentChoreSystem/src/UserInterface/LoginPage.java
@@ -8,7 +8,6 @@ import java.io.IOException;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
-import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
