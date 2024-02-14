9cc475eb0e7ba27b7ee58c6bd2678efd7c672e88
NathanBinu <Nathanbinu011@gmail.com>
2024-02-14 16:24:56 -0500

    Update CompetitionTest.java
    finished CompetitionTest

--- ParentChoreSystem/src/Tests/CompetitionTest.java
+++ ParentChoreSystem/src/Tests/CompetitionTest.java
@@ -4,32 +4,64 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import accountsModule.ChildAccount;
+import accountsModule.ParentAccount;
 import choresModule.Chore;
 import competitionModule.Competition;
 
 class CompetitionTest {
 	
-	Competition competition;
-	ChildAccount child1;
-    ChildAccount child2;
-    List<Chore> chores1;
-    List<Chore> chores2;
-
+	private ParentAccount parent;
+	private ChildAccount child1;
+    private ChildAccount child2;
+    private Chore chore;
+    private double child1HoursAtStart;
+    private double child2HoursAtStart;
 	
 	@BeforeEach
 	
-	void init() {
-		competition = new Competition();
-	}
+	public void setUp() {
+		parent = new ParentAccount("parent", "parentpass");
+        child1 = new ChildAccount("child1", "child1pass");
+        child2 = new ChildAccount("child2", "child2pass");
+        chore = new Chore("Clean Bedroom", "Household", 60, 10.0);
+        parent.addChildAccount(child1);
+        parent.addChildAccount(child2);
 
+        
+        child1HoursAtStart = child1.getHoursWorked();
+        child2HoursAtStart = child2.getHoursWorked();
+	}
+	
+	@AfterEach
+	public void tearDown() throws Exception {
+		
+		parent = null;
+		child1 = null;
+		child2 = null;
+		chore = null;
+
+		
+	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+    public void testGetWinner_Child1Wins() {
+		Competition competition = new Competition(parent, child1, child2, chore);
+        child1.markChoreCompleted(chore);
+        assertEquals(child1, competition.getWinner());
+    }
+	
+	
+	@Test
+    public void testGetWinner_Child2Wins() {
+		Competition competition = new Competition(parent, child1, child2, chore);
+        child2.markChoreCompleted(chore);
+        assertEquals(child2, competition.getWinner());
+    }
+	
 
 }
