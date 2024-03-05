43e3e0a0e0bc260e0586e45262c8e42467607c39
NathanBinu <Nathanbinu011@gmail.com>
2024-03-05 16:48:32 -0500

    changes to competition class
    allowing for competition between more children in a family - still needs improvements

--- ParentChoreSystem/src/competitionModule/Competition.java
+++ ParentChoreSystem/src/competitionModule/Competition.java
@@ -12,12 +12,13 @@ public class Competition {
 	
 //double rewardPoints1; //storing rewards points 1
 //double rewardPoints2; //storing rewards points 2
-private ParentAccount parent;
-private ChildAccount child1; //child account 1 declaration
-private ChildAccount child2; //child account 2 declaration
-private final double CHILD1_HOURS_TILL_NOW;
-private final double CHILD2_HOURS_TILL_NOW;
+//private ParentAccount parent;
+//private ChildAccount child1; //child account 1 declaration
+//private ChildAccount child2; //child account 2 declaration
+//private final double CHILD1_HOURS_TILL_NOW;
+//private final double CHILD2_HOURS_TILL_NOW;
 private Chore chore;
+private ArrayList<ChildAccount> children =  new ArrayList<ChildAccount>();
 
 
     
@@ -26,16 +27,28 @@ private Chore chore;
      * @param child1 ChildAccount object representing child 1
      * @param child2 ChildAccount object representing child 2
      */
-	public Competition(ParentAccount parent, ChildAccount child1, ChildAccount child2, Chore chore) {
-		this.parent = parent;
-		this.child1 = child1;  //Assigning child 1 account
-		this.child2 = child2;  //Assigning child 2 account
-		this.CHILD1_HOURS_TILL_NOW = child1.getHoursWorked();    
-		this.CHILD2_HOURS_TILL_NOW = child2.getHoursWorked();    
-		this.chore = chore;
-		parent.assignChore(child1, chore);  //initializing reward points 1 
-		parent.assignChore(child2, chore);  //initializing reward points 2 
-	}
+	 public Competition(ParentAccount parent, ArrayList<ChildAccount> children, Chore chore) {
+		 //this.parent = parent;
+		 this.children = children;
+		 for (int i = 0; i<children.size();i++) {
+			 //this.CHILD_HOURS_TILL_NOW = children.get(i).getHoursWorked();
+			 parent.assignChore(children.get(i), chore);  //initializing 
+		 }
+		 this.chore = chore;
+	 	}
+
+
+
+//	public Competition(ParentAccount parent, ChildAccount child1, ChildAccount child2, Chore chore) {
+//		this.parent = parent;
+//		this.child1 = child1;  //Assigning child 1 account
+//		this.child2 = child2;  //Assigning child 2 account
+//		this.CHILD1_HOURS_TILL_NOW = child1.getHoursWorked();    
+//		this.CHILD2_HOURS_TILL_NOW = child2.getHoursWorked();    
+//		this.chore = chore;
+//		parent.assignChore(child1, chore);  //initializing reward points 1 
+//		parent.assignChore(child2, chore);  //initializing reward points 2 
+//	}
 	
 	//rewardPoints1 = 0
 	//rewardPoints2 = 0
