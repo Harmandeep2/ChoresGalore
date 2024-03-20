7d44012d680bd8d9eb4b8d87932901261bac7b23
Ayan Saiyed <ayan.unit8@gmail.com>
2024-03-19 20:24:11 -0400

    Update Chore.java

--- ParentChoreSystem/src/choresModule/Chore.java
+++ ParentChoreSystem/src/choresModule/Chore.java
@@ -2,7 +2,7 @@ package choresModule;
 
 
 public class Chore {
-	
+	//instance variables
 	private int id;
 	private String name;				//case sensitive
 	private String category;			//case sensitive
@@ -12,7 +12,14 @@ public class Chore {
 	private boolean completed;			//status of chore, true = completed, false = incomplete
 	private boolean paid;				//status of payment, true = paid, false = unpaid
 	
-	//Constructors
+
+	/**
+	 * Constructor
+	 * @param name
+	 * @param category
+	 * @param time
+	 * @param payment
+	 */
 	public Chore(String name, String category, double time, double payment) {
 		this.name = name;
 		this.category = category;
@@ -23,45 +30,107 @@ public class Chore {
 	}
 	
 	//Getters and Setters
+	
+	/**
+	 * getId method for choreID
+	 * @return id
+	 */
 	public int getId() {
 		return id;
 	}
+	
+	/**
+	 * setID of chore
+	 * @param id
+	 */
 	public void setId(int id) {
 		this.id = id;
 	}
+	
+	/**
+	 * getName of chore
+	 * @return name
+	 */
 	public String getName() {
 		return name;
 	}
+	
+	/**
+	 * setName for chore
+	 * @param name
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+	/**
+	 * getCategory of chore
+	 * @return category
+	 */
 	public String getCategory() {
 		return category;
 	}
+	
+	/**
+	 * setCategory of chore
+	 * @param category
+	 */
 	public void setCategory(String category) {
 		this.category = category;
 	}
+	
+	/**
+	 * getTime required to finish chore
+	 * @return time
+	 */
 	public double getTime() {
 	    return time;
 	}
+	
+	/**
+	 * setTime for chore
+	 * @param time
+	 */
 	public void setTime(double time) {
 	    this.time = time;
 	}
+	
+	/**
+	 * getPayment for chore
+	 * @return payment
+	 */
 	public double getPayment() {
 	    return payment;
 	}
+	
+	/**
+	 * setPaymeny for chore
+	 * @param payment
+	 */
 	public void setPayment(double payment) {
 	    this.payment = payment;
 	}
 	
+	/**
+	 * get username of assigned parent
+	 * @return parentUsername
+	 */
 	public String getParentUsername() {
 		return parentUsername;
 	}
 
+	/**
+	 * set name for assigned parent
+	 * @param parentUsername
+	 */
 	public void setParentUsername(String parentUsername) {
 		this.parentUsername = parentUsername;
 	}
 	
+	/**
+	 * checks if chore completed
+	 * @return completed
+	 */
 	public boolean isCompleted() {
 		return completed;
 	}
@@ -70,10 +139,14 @@ public class Chore {
 		completed = true;
 	}
 	
+	/**
+	 * isPaid method to make sure a chore is paid
+	 * @return paid
+	 */
 	public boolean isPaid() {
 		return paid;
 	}
-	
+
 	public void markPaid() {
 		paid = true;
 	}
